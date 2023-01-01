487a09e1e1ff5a027aef6172b193aaa64376e368
Laky64 <iraci.matteo@gmail.com>
2023-01-01 14:45:50 +0100

    Added EmojiPack file size info

--- handlers/emoji_packs.go
+++ handlers/emoji_packs.go
@@ -19,11 +19,12 @@ func EmojiPacks(ctx *fasthttp.RequestCtx, clientEmoji *emoji.Context) {
 	var result []types.PacksInfo
 	for _, pack := range clientEmoji.EmojiPacks {
 		result = append(result, types.PacksInfo{
-			Name:    pack.DisplayName,
-			Version: pack.Version,
-			Id:      pack.Identifier,
-			Preview: fmt.Sprintf("%s/previews/%s.png?v=%d", consts.ServerBase, pack.Identifier, pack.Version),
-			File:    fmt.Sprintf("%s/packs/%s.zip?v=%d", consts.ServerBase, pack.Identifier, pack.Version),
+			Name:     pack.DisplayName,
+			Version:  pack.Version,
+			Id:       pack.Identifier,
+			Preview:  fmt.Sprintf("%s/previews/%s.png?v=%d", consts.ServerBase, pack.Identifier, pack.Version),
+			File:     fmt.Sprintf("%s/packs/%s.zip?v=%d", consts.ServerBase, pack.Identifier, pack.Version),
+			FileSize: len(pack.Emojies),
 		})
 	}
 	res, _ := json.Marshal(result)
--- telegram/emoji/types/packs_info.go
+++ telegram/emoji/types/packs_info.go
@@ -1,9 +1,10 @@
 package types
 
 type PacksInfo struct {
-	Name    string `json:"name"`
-	Version int    `json:"version"`
-	Id      string `json:"id"`
-	Preview string `json:"preview"`
-	File    string `json:"file"`
+	Name     string `json:"name"`
+	Version  int    `json:"version"`
+	Id       string `json:"id"`
+	Preview  string `json:"preview"`
+	File     string `json:"file"`
+	FileSize int    `json:"file_size"`
 }
