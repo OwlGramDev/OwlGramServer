0be68b07bccb891560aafb0dd5849abc911f796b
Laky64 <iraci.matteo@gmail.com>
2023-01-07 19:56:30 +0100

    Fixed some build problems python side

--- libs/apks_sender/__main__.py
+++ libs/apks_sender/__main__.py
@@ -12,6 +12,9 @@ from utils import Utils
 async def __main__():
     print("[*] Starting APKs sender...")
     current_path = os.path.abspath(os.path.join(__file__, "../"))
+    cache_path = os.path.join(current_path, "cache")
+    if not os.path.exists(cache_path):
+        os.mkdir(cache_path)
     data = json.loads(bytes.fromhex(sys.argv[1]).decode('utf-8'))
     utils = Utils(current_path)
     client = Client('OwlGram', data['api_id'], data['api_hash'], bot_token=data['bot_token'])
